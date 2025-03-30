393a55e6bc823f2e87bcad9071fb1b24afd0e72b
chsedd <83226177+chsedd@users.noreply.github.com>
2025-03-30 14:34:52 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu primeiro nome: ");
+        string Nome = Console.ReadLine();
+        Console.Write("Digite seu sobrenome: ");
+        string Sobrenome = Console.ReadLine();
+
+        Console.WriteLine("Nome Completo: " + Nome + " " + Sobrenome);
+
+        Console.WriteLine("Nome de catálogo: " + Sobrenome + ", " + Nome);
+        
+    }
+}
